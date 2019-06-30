8eb329c945c2faddbb9ac51e684bea65f52bffd5
Ledion Bitincka <lbitincka@gmail.com>
2019-06-29 23:28:48 -0700

    again ...

--- src/NodeBuilder.js
+++ src/NodeBuilder.js
@@ -188,7 +188,7 @@ class NodeJsBuilder {
   buildFromSource(){
     const makeArgs = isWindows ? ['nosign', 'release'] : [`-j${os.cpus().length}`];
     if(isWindows) {
-      runCommand('dir');
+      runCommand('fsutil', ['volume', 'diskfree', 'd:']);
     }
     return this.downloadExpandNodeSource()
       .then(() => this.prepareNodeJsBuild())
