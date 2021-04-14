4b091b73804e674492d08f2a3e4a20679480eae9
Clint Sharp <clint@cribl.io>
2021-04-14 20:17:54 +0000

    removing conditions for azure pipelines, removing armv7 in build builder

--- build-builder.sh
+++ build-builder.sh
@@ -4,6 +4,6 @@ if [ $(docker buildx ls | grep js2bin-builder  | wc -l) -eq 0 ]; then
     docker buildx create --name js2bin-builder
 fi
 docker buildx use js2bin-builder
-docker buildx build -t "cribl/js2bin-builder:latest-nonx64" --platform linux/arm64/v8,linux/arm/v7 --push -f Dockerfile.ubuntu .
+docker buildx build -t "cribl/js2bin-builder:latest-nonx64" --platform linux/arm64/v8 --push -f Dockerfile.ubuntu .
 docker build -t "cribl/js2bin-builder:latest" -f Dockerfile.centos6 .
 docker push "cribl/js2bin-builder:latest"
