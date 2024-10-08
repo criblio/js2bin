ee1e52f72f1a7cb88c1d789fbec21e3aaea7199f
Amir Valizadeh <avalizadeh@cribl.io>
2024-10-08 15:53:29 -0700

    upgraded nodejs version to 20.18.0

--- build-builder.sh
+++ build-builder.sh
@@ -5,6 +5,7 @@ if [ $(docker buildx ls | grep js2bin-builder  | wc -l) -eq 0 ]; then
 fi
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx use js2bin-builder
-docker buildx build -t "cribl/js2bin-builder:latest-nonx64" --platform linux/arm64/v8 --push -f Dockerfile.centos7.arm64 .
-docker build -t "cribl/js2bin-builder:latest" -f Dockerfile.centos7 .
-docker push "cribl/js2bin-builder:latest"
+BUILDER_IMAGE_VERSION="2"
+docker buildx build -t "cribl/js2bin-builder:${BUILDER_IMAGE_VERSION}-nonx64" --platform linux/arm64/v8 --push -f Dockerfile.centos7.arm64 .
+docker build -t "cribl/js2bin-builder:${BUILDER_IMAGE_VERSION}" -f Dockerfile.centos7 .
+docker push "cribl/js2bin-builder:${BUILDER_IMAGE_VERSION}"
