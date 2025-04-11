33ccb0603a00ec4b2b258ed81087822097d96b55
Michal Biesek <mbiesek@cribl.io>
2025-04-11 14:47:31 +0200

    Use buildx for both builds in build-builder.sh
    Allow builds on arm64 host instance

--- build-builder.sh
+++ build-builder.sh
@@ -6,6 +6,11 @@ fi
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx use js2bin-builder
 BUILDER_IMAGE_VERSION="3"
+ARCH=$(uname -m)
+if [ "$ARCH" == "x86_64" ]; then
+    echo "WARNING: You are running on an x86_64 host. Cross-compiling Python for arm64 may result in build failures or inconsistent behavior."
+    echo "It is recommended to run this script on an arm64 host if possible."
+fi
 docker buildx build -t "cribl/js2bin-builder:${BUILDER_IMAGE_VERSION}-nonx64" --platform linux/arm64/v8 --push -f Dockerfile.centos7.arm64 .
-docker build -t "cribl/js2bin-builder:${BUILDER_IMAGE_VERSION}" -f Dockerfile.centos7 .
-docker push "cribl/js2bin-builder:${BUILDER_IMAGE_VERSION}"
+docker buildx build -t "cribl/js2bin-builder:${BUILDER_IMAGE_VERSION}" --platform linux/amd64 --push -f Dockerfile.centos7 .
+
